c863c86fcddad9730fa1acbf023b5869add9ece5
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-03-24 23:49:34 -0500

    binarySearch

--- Workbook03/BinarySearch.java
+++ Workbook03/BinarySearch.java
@@ -1,31 +1,86 @@
 package Workbook03;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class BinarySearch {
 
   /** Variables */
+  private int[] array = new int[20];
 
   /** Constructors */
 
   /** Setters */
+  public void setArray() {
+    for (int i = 0; i < array.length; i++) {
+      this.array[i] = new Random().nextInt(50) + 1;
+    }
+  }
 
   /** Getters */
+  public String toString() {
+
+    StringBuilder strArray = new StringBuilder();
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.print("-----");
+    }
+    System.out.println();
+    for (int i = 0; i < array.length; i++) {
+      strArray.append(" | " + array[i]);
+    }
+
+    return strArray.toString();
+  }
 
   /** Methods */
-  public int binarySearch(int[] arr, int key) {
+  public int binarySearch(int key) {
     int start = 0;
-    int end = arr.length - 1;
-    int mid = (int) Math.floor(arr.length / 2);
+    int end = array.length - 1;
+    int mid = (int) Math.floor(array.length / 2);
 
-    while (arr[mid] != key && start <= end) {
+    while (array[mid] != key && start <= end) {
 
-      if (key < arr[mid])
+      if (key < array[mid])
         end = mid - 1;
       else
         start = mid + 1;
       mid = (int) Math.floor((start + end) / 2);
     }
 
-    return key == arr[mid] ? mid : -1;
+    return key == array[mid] ? mid : -1;
+  }
+
+  public void bubbleSort() {
+    for (int i = this.array.length - 1; i > 0; i--) {
+      boolean noSwaps = true;
+      for (int j = 0; j < i; j++) {
+        if (this.array[j] > this.array[j + 1]) {
+          var temp = this.array[j];
+          this.array[j] = this.array[j + 1];
+          this.array[j + 1] = temp;
+          noSwaps = false;
+        }
+      }
+      if (noSwaps)
+        break;
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    BinarySearch s = new BinarySearch();
+
+    s.setArray();
+    System.out.printf("%s\n\n", s.toString());
+
+    s.bubbleSort();
+    System.out.printf("%s\n\n", s.toString());
+
+    System.out.print("Search value: ");
+    System.out.printf("The value is: %d", s.binarySearch(input.nextInt()));
+
+    input.close();
   }
 
 }
