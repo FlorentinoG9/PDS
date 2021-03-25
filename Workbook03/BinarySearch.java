3542d2310f46143b2357e33815ff398db4099748
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-03-24 23:19:38 -0500

    Added Binary Search

--- Workbook03/BinarySearch.java
+++ Workbook03/BinarySearch.java
@@ -9,7 +9,23 @@ public class BinarySearch {
   /** Setters */
 
   /** Getters */
-  
+
   /** Methods */
-  
+  public int binarySearch(int[] arr, int key) {
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = (int) Math.floor(arr.length / 2);
+
+    while (arr[mid] != key && start <= end) {
+
+      if (key < arr[mid])
+        end = mid - 1;
+      else
+        start = mid + 1;
+      mid = (int) Math.floor((start + end) / 2);
+    }
+
+    return key == arr[mid] ? mid : -1;
+  }
+
 }
