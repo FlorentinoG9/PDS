10101569d9dd067fa1c48a2c7624864da2409763
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-04-29 05:23:36 -0500

    Finish Comments WB7

--- Workbook07/TestMergeSort.java
+++ Workbook07/TestMergeSort.java
@@ -9,30 +9,54 @@ public class TestMergeSort {
     /** create object of MergeSort O(n Log n) */
     MergeSort mSort = new MergeSort();
     /** Create the array as a Lsit objects */
-    List<Integer> arr = new ArrayList<Integer>();
+    List<Integer> arr1 = new ArrayList<Integer>();
     // { 2, 3, 2, 5, 6, 1, -2, 3, 14, 12 };
+    List<Integer> arr2 = new ArrayList<Integer>();
+    // list2 {8,3,4,5,22,-5,-2,7,1,18} 
 
     /** Init the Lsit array with the values given */
-    arr.add(2);
-    arr.add(3);
-    arr.add(2);
-    arr.add(5);
-    arr.add(6);
-    arr.add(1);
-    arr.add(-1);
-    arr.add(3);
-    arr.add(14);
-    arr.add(12);
+    arr1.add(2);
+    arr1.add(3);
+    arr1.add(2);
+    arr1.add(5);
+    arr1.add(6);
+    arr1.add(1);
+    arr1.add(-1);
+    arr1.add(3);
+    arr1.add(14);
+    arr1.add(12);
 
+    /** Init the Lsit array with the values given */
+    arr2.add(8);
+    arr2.add(3);
+    arr2.add(4);
+    arr2.add(5);
+    arr2.add(22);
+    arr2.add(-5);
+    arr2.add(-2);
+    arr2.add(7);
+    arr2.add(1);
+    arr2.add(18);
+
+    /** Array 1 */
     /** print the List Unsorted */
+    System.out.println("Array 1:");
     System.out.print("Unsorted: "); 
-    printArrayList(arr);
-
+    printArrayList(arr1);
     /** Print the Sorted List */
     System.out.print("Sorted: ");
-    
     /** pass the array list as a parameter */
-    printArrayList(mSort.mergeSort(arr));
+    printArrayList(mSort.mergeSort(arr1));
+    
+    System.out.println();
+    
+    /** Array 2 */
+    System.out.println("Array 2:");
+    System.out.print("Unsorted: "); 
+    printArrayList(arr2);
+    System.out.print("Sorted: ");
+    printArrayList(mSort.mergeSort(arr2));
+
 
   }
 
--- Workbook07/TestQuickSort.java
+++ Workbook07/TestQuickSort.java
@@ -3,12 +3,19 @@ package Workbook07;
 public class TestQuickSort {
   public static void main(String[] args) {
     // Init the array
-    int[] arr = { 2, 3, 2, 5, 6, 1, -2, 3, 14, 12 };
+    int[] arr1 = { 2, 3, 2, 5, 6, 1, -2, 3, 14, 12 };
+    int[] arr2 = {8,3,4,5,22,-5,-2,7,1,18};
     
     // create the Quicksort object and pass through the array
-    QuickSort qs = new QuickSort(arr);
-
+    System.out.println("Array 1:");
     // sort the array by calling the method quickSort()
-    qs.quickSort();
+    QuickSort qs1 = new QuickSort(arr1);
+    qs1.quickSort();
+
+
+    System.out.println("\nArray 2:");
+    QuickSort qs2 = new QuickSort(arr2);
+    qs2.quickSort();
+
   }
 }
